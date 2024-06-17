32aa0eb05d86d0295910e87da2e64f82831bfa34
Menet Mickaël <mickaelmenet@gmail.com>
2024-06-17 10:49:04 +0200

    fix logementDetails code

--- src/components/logement/LogementDetails.tsx
+++ src/components/logement/LogementDetails.tsx
@@ -13,32 +13,43 @@ type LogementDetailsProps = {
     };
 };
 
-const LogementDetails = ({ data }: LogementDetailsProps) => (
-    <CallOut title={`Informations sur le logement ${data.logement}`}>
-        <ul style={styles.ul}>
-            <li style={styles.li}>
-                <i className="ri-home-line" style={styles.icon} /> Logement: {data.logement}
-            </li>
-            <li style={styles.li}>
-                <i className="ri-calendar-line" style={styles.icon} /> Trimestre: {data.trimestre}
-            </li>
-            <li style={styles.li}>
-                <i className="ri-map-pin-line" style={styles.icon} /> Rges: {data.rges}
-            </li>
-            <li style={styles.li}>
-                <i className="ri-barcode-line" style={styles.icon} /> NOENQ: {data.noenq}
-            </li>
-            <li style={styles.li}>
-                <i className="ri-government-line" style={styles.icon} /> DEP: {data.dep}
-            </li>
-            <li style={styles.li}>
-                <i className="ri-building-line" style={styles.icon} /> Commune: {data.commune}
-            </li>
-            <li style={styles.li}>
-                <i className="ri-road-map-line" style={styles.icon} /> Adresse: {data.adresse}
-            </li>
-        </ul>
-    </CallOut>
+type InfoItemProps = {
+    iconClass: string;
+    label: string;
+    value: string;
+};
+
+const InfoItem = ({ iconClass, label, value }: InfoItemProps) => (
+    <li style={styles.li}>
+        <i className={iconClass} style={styles.icon} /> {label}: {value}
+    </li>
 );
+
+const LogementDetails = ({ data }: LogementDetailsProps) => {
+    const infoItems = [
+        { iconClass: "ri-home-line", label: "Logement", value: data.logement },
+        { iconClass: "ri-calendar-line", label: "Trimestre", value: data.trimestre },
+        { iconClass: "ri-map-pin-line", label: "Rges", value: data.rges },
+        { iconClass: "ri-barcode-line", label: "NOENQ", value: data.noenq },
+        { iconClass: "ri-government-line", label: "DEP", value: data.dep },
+        { iconClass: "ri-building-line", label: "Commune", value: data.commune },
+        { iconClass: "ri-road-map-line", label: "Adresse", value: data.adresse }
+    ];
+
+    return (
+        <CallOut title={`Informations sur le logement ${data.logement}`}>
+            <ul style={styles.ul}>
+                {infoItems.map((item, index) => (
+                    <InfoItem
+                        key={index}
+                        iconClass={item.iconClass}
+                        label={item.label}
+                        value={item.value}
+                    />
+                ))}
+            </ul>
+        </CallOut>
+    );
+};
 
 export default LogementDetails;
