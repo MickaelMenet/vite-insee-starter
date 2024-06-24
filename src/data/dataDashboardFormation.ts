3898f3b1c910ffb8e7c2d639d1be520d8e9677b3
Menet Mickaël <mickaelmenet@gmail.com>
2024-06-24 10:05:28 +0200

    change data model

---
+++ src/data/dataDashboardFormation.ts
@@ -0,0 +1,16 @@
+export const tableDataFormation = [
+    ["2024T1", "Nord-Pas-de-Calais", "1", "0", "1", "MEL-001"],
+    ["2024T1", "Rhône-Alpes", "1", "0", "1", "MEL-002"],
+    ["2024T2", "Réunion", "3", "0", "2", "MEL-003"],
+    ["2024T2", "Basse-Normandie", "1", "0", "1", "MEL-004"],
+    ["2024T2", "Bourgogne", "1", "0", "1", "MEL-005"],
+    ["2024T2", "Lorraine", "1", "0", "1", "MEL-006"],
+    ["2024T2", "Franche-Comté", "1", "0", "1", "MEL-007"],
+    ["2024T2", "Pays de la Loire", "1", "0", "1", "MEL-008"],
+    ["2024T2", "Bretagne", "1", "0", "1", "MEL-009"],
+    ["2024T2", "Poitou-Charentes", "1", "0", "1", "MEL-010"],
+    ["2024T2", "Rhône-Alpes", "6", "0", "6", "MEL-011"],
+    ["2024T2", "Languedoc-Roussillon", "2", "0", "2", "MEL-012"],
+    ["2024T2", "Provence-Alpes-Côte d’Azur", "3", "0", "3", "MEL-013"],
+    ["2024T2", "Corse", "2", "0", "1", "MEL-014"]
+];
