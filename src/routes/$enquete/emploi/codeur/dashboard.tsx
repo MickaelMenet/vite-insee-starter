be547daa43e33f66a3f80b28b9ee98f01d6057e2
Menet Mickaël <mickaelmenet@gmail.com>
2024-06-24 10:06:02 +0200

    modify all dashboard

--- src/routes/$enquete/emploi/codeur/dashboard.tsx
+++ src/routes/$enquete/emploi/codeur/dashboard.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import DashboardTable from "../../../../components/DashboardTable";
-import { tableData } from "../../../../data/dataEmploi";
+import { tableData } from "../../../../data/dataDashboardEmploi";
 
 export const Route = createFileRoute("/$enquete/emploi/codeur/dashboard")({
     component: () => {
